e392f833d84f76b265a09160b9dcfae2115a6f58
Gregory Burlet <gregory.burlet@gmail.com>
2014-03-23 22:20:13 -0600

    Binary classification PMTK3 experiment
    Trying out the PMTK3 hmmFit function.

--- binaryclassification.m
+++ binaryclassification.m
@@ -15,22 +15,38 @@ clearvars -except X y % memory management
 
 % experiment parameters
 train_percent = 0.8;
-classes = {0, 1:3};      % {'healthy': 0, 'ADHD': 1}
-num_states = 4;          % TODO: choose this using k-fold cross validation
+classes = {0, 1:3};             % {'healthy': 0, 'ADHD': 1}
+num_classes = length(classes);
+num_states = 4;                 % TODO: choose this using k-fold cross validation
 
 [Xtrain, Xtest, ytrain, ytest] = partitiondata(X, y, train_percent, classes);
-[N, T, emission.d] = size(Xtrain);
+[N, T, F] = size(Xtrain);
 
-% initialize HMM
+% initialize HMMs
 % equiprobable state distributions
-pi = ones(num_states, 1)/num_states;
-A = ones(num_states)/num_states;
+pi0 = ones(num_states, 1)/num_states;
+A0 = ones(num_states)/num_states;
 
 % initialize emission distributions using MLE from data
 % Use multivariate gaussian emission distribution
-% TODO: implement GMM emission distribution
 % Note: each state multivariate gaussian distribution has equivalent parameters
-Xtrain_tempavg = squeeze(mean(X,2));
-emission.mu = repmat(mean(Xtrain_tempavg, 1), [num_states, 1])';
-emission.Sigma = repmat(cov(Xtrain_tempavg), [1,1,num_states]);
-hmm = hmmCreate('gauss', pi, A, emission);
+% TODO: implement GMM emission distribution
+hmms = cell(1, num_classes);
+for class = 0:num_classes-1
+    % gather observations for the data samples with this class label
+    % preprocess the observations to be used by the EM learning function
+    Obs = permute(X(y == class,:,:), [1,3,2]);
+    Obs = cellfun(@squeeze, num2cell(Obs, [2,3]), 'UniformOutput', false);
+    
+    % initialize
+    Xtrain_tempavg = squeeze(mean(X(y == class,:,:),2));
+    mu0 = repmat(mean(Xtrain_tempavg, 1), [num_states, 1])';
+    Sigma0 = repmat(cov(Xtrain_tempavg), [1,1,num_states]);
+    emission0 = condGaussCpdCreate(mu0, Sigma0); 
+
+    % train HMM
+    hmms{class+1} = hmmFit(Obs, num_states, 'gauss', ...
+                    'pi0', pi0, 'trans0', A0, 'emission0', emission0, ...
+                    'maxIter', 1000, 'convTol', 1e-7, ...
+                    'nRandomRestarts', 3, 'verbose', true);
+end
