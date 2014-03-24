01895512f2bb353df9779b07e9599ee57e070fde
gburlet <gregory.burlet@mail.mcgill.ca>
2014-03-23 23:59:49 -0600

    classification accuracy reported
    report classification accuracy. Bhaskar's code almost integrated. Next
    up: 5-fold cross-validation.

--- binaryclassification.m
+++ binaryclassification.m
@@ -15,9 +15,10 @@ clearvars -except X y % memory management
 
 % experiment parameters
 train_percent = 0.8;
-classes = {0, 1:3};             % {'healthy': 0, 'ADHD': 1}
+%classes = {0, 1:3};             % {'healthy': 0, 'ADHD': 1}
+classes = {0, 1, 3};
 num_classes = length(classes);
-num_states = 4;                 % TODO: choose this using k-fold cross validation
+num_states = 15;                 % TODO: choose this using k-fold cross validation
 
 [Xtrain, Xtest, ytrain, ytest] = partitiondata(X, y, train_percent, classes);
 [N, T, F] = size(Xtrain);
@@ -35,18 +36,32 @@ hmms = cell(1, num_classes);
 for class = 0:num_classes-1
     % gather observations for the data samples with this class label
     % preprocess the observations to be used by the EM learning function
-    Obs = permute(X(y == class,:,:), [1,3,2]);
+    Obs = permute(Xtrain(ytrain == class,:,:), [1,3,2]);
     Obs = cellfun(@squeeze, num2cell(Obs, [2,3]), 'UniformOutput', false);
     
     % initialize
-    Xtrain_tempavg = squeeze(mean(X(y == class,:,:),2));
+    Xtrain_tempavg = squeeze(mean(Xtrain(ytrain == class,:,:),2));
     mu0 = repmat(mean(Xtrain_tempavg, 1), [num_states, 1])';
     Sigma0 = repmat(cov(Xtrain_tempavg), [1,1,num_states]);
     emission0 = condGaussCpdCreate(mu0, Sigma0); 
 
     % train HMM
     hmms{class+1} = hmmFit(Obs, num_states, 'gauss', ...
-                    'pi0', pi0, 'trans0', A0, 'emission0', emission0, ...
                     'maxIter', 1000, 'convTol', 1e-7, ...
-                    'nRandomRestarts', 3, 'verbose', true);
+                    'nRandomRestarts', 1, 'verbose', true, ...
+                    'pi0', pi0, 'trans0', A0, 'emission0', emission0);
 end
+
+% evaluate model on testing data
+num_test = length(ytest);
+Obs_test = permute(Xtest, [1,3,2]);
+Obs_test = cellfun(@squeeze, num2cell(Obs_test, [2,3]), 'UniformOutput', false);
+logp = zeros(num_test, num_classes);
+for class = 1:num_classes
+     logp(:,class) = hmmLogprob(hmms{class}, Obs_test);
+end
+[~, yhat] = max(logp, [], 2);
+recall = sum(yhat-1 == ytest);
+accuracy = recall / num_test;
+
+fprintf('Classification accuracy %d/%d: %.2f%% \n', recall, num_test, accuracy);
